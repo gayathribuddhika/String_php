a8cba26b326d001d258e59b947735e27cee4c46e
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-19 11:32:27 +0530

    String_php
    String declaration in php

---
+++ string.php
@@ -0,0 +1,19 @@
+<?php
+$first_name = "Gayathri ";
+echo $first_name;
+
+$last_name = "Buddhika";
+echo $last_name;
+
+echo "<br>";
+echo $first_name." ".$last_name;
+
+echo "<br>";
+echo "Full Name : " . $first_name . " " . $last_name;
+
+echo "<br>";
+echo "Full Name : $first_name $last_name";
+
+echo "<br>";
+echo "Full Name : {$first_name} {$last_name}";
+?>
